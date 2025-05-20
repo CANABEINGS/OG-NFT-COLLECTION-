1e2fd73b82834d241e59f8faf1fe36f0b15a4737
$CANA <canabeingsofficial@gmail.com>
2025-05-20 15:08:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// This file is intentionally left minimal as per requirements
+// Additional functionality can be added here if needed in the future
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Future JavaScript functionality can be added here
+    console.log('OG NFT Collection page loaded');
+});
